4137724dbe12a0ce75b117389bb44c6a33fc37ec
William Torres <william.torres@quarkdo.com>
2023-11-19 13:29:58 -0300

    Body of detail

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,14 +4,14 @@ import {Profile} from "./components/Profile";
 import {Searchbar} from "./components/Searchbar";
 import {Publications} from "./components/Publications";
 import {useEffect, useState} from "react";
-import {IssueItems, SearchInssueIntoRepository} from "../../repository/github.com";
+import {IssueItems, SearchIssueIntoRepository} from "../../repository/github.com";
 
 export function Home() {
     const [searchQuery, setSearchQuery] = useState('');
     const [publications, setPublications] = useState([] as IssueItems);
 
     useEffect(() => {
-        SearchInssueIntoRepository(searchQuery).then(function ({data}) {
+        SearchIssueIntoRepository(searchQuery).then(function ({data}) {
             setPublications(data.items);
         })
     }, [searchQuery])
--- src/pages/Issue/index.tsx
+++ src/pages/Issue/index.tsx
@@ -1,14 +1,37 @@
 import {Header} from "../../components/Header";
 import {Container} from "../../components/ContentContainer";
 import {Title} from "./components/Title";
+import {Content} from "./components/Content";
+import {GetIssue, IssueItem} from "../../repository/github.com";
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 
 
 export function  Issue(){
+    const params = useParams();
+    const [issue, setIssue] = useState<IssueItem>({} as IssueItem);
+    useEffect(() => {
+        if (params.issueId) {
+            GetIssue(params.issueId).then(({data}) => {
+                setIssue(data);
+            })
+        }
+    }, [params]);
+
+    if (!issue.url) {
+        return (
+            <Container>
+                <Header/>
+                <h1>Carregando</h1>
+            </Container>
+        )
+    }
     return (
         <>
             <Header />
             <Container>
-                <Title></Title>
+                <Title issue={issue} />
+                <Content issue={issue} />
             </Container>
         </>
     )
